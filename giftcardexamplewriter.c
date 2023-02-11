e53de0a1162d3f68c89d10b66b17cc1e0d37d546
RajShah <ps4341@nyu.edu>
2023-02-11 13:06:56 -0500

    commit

--- giftcardexamplewriter.c
+++ giftcardexamplewriter.c
@@ -39,7 +39,7 @@ void setupgc() {
 	examplegcrd.actual_record = (void *) &examplegcac;
 	examplegcac.amount_added = 2000;
 	//Adding more than the required 32 bits of allocated data... Crash2?
-	examplegcac.actual_signature = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+	examplegcac.actual_signature = "[ insert crypto signature here ]";
 }
 
 
